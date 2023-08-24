1007ff858ee97d7abd8e75389ef9bbc57180fbc0
KavindaS <KavindaS@99X-SL-PF2N967A>
2023-08-25 04:12:28 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+// app.component.ts
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'team-dashboard';
+  data: any[] = [];
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const workbook = XLSX.read(e.target.result, { type: 'binary' });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      this.data = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+    };
+    reader.readAsBinaryString(file);
+  }
 }
